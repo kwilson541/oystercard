90d36a035550674e2954924af599c2d19c9dee5e
Kim Wilson <k.wilson541@gmail.com>
2016-11-17 22:22:29 +0000

    Added methods and tests to Journey Class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,5 +1,31 @@
 class Journey
 
+	PENALTY_FARE = 6
+	MINIMUM_FARE = 1
 
+	attr_reader :entry_station, :exit_station
+
+	def initialize
+		@entry_station
+		@exit_station
+	end
+
+	def start_journey(station)
+		@entry_station = station
+	end
+
+	def end_journey(station)
+		@exit_station = station
+		self
+	end
+
+	def fare
+		return PENALTY_FARE unless complete?
+		MINIMUM_FARE
+	end
+
+	def complete?
+		entry_station != nil && exit_station != nil
+	end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,6 +2,59 @@ require 'journey'
 
 describe Journey do
 
+	subject(:journey) { described_class.new }
+	let(:station) { double(:station) }
 
+	it 'has an entry station when a journey is started' do
+		journey.start_journey(station)
+		expect(journey.entry_station).to eq station
+	end
+
+	it 'has an exit station when a journey is ended' do
+		journey.end_journey(station)
+		expect(journey.exit_station).to eq station
+	end
+
+	it 'returns itself when journey is ended' do
+		expect(journey.end_journey(station)).to eq journey
+	end
+
+	describe '#complete' do
+
+		it 'knows if a journey is incomplete' do
+			expect(journey).not_to be_complete
+		end
+
+		it 'knows if a journey is complete' do
+			journey.start_journey(station)
+			journey.end_journey(station)
+			expect(journey).to be_complete
+		end
+
+	end
+
+	describe '#fare' do
+
+		it 'has a default fare of penalty fare' do
+			expect(journey.fare).to eq Journey::PENALTY_FARE
+		end
+
+		it 'calculates a fare' do
+			journey.start_journey(station)
+			journey.end_journey(station)
+			expect(journey.fare).to eq 1
+		end
+
+		it 'gives a penalty fare if exit station is missing' do
+			journey.start_journey(station)
+			expect(journey.fare).to eq Journey::PENALTY_FARE
+		end
+
+		it 'gives a penalty fare if entry station is missing' do
+			journey.end_journey(station)
+			expect(journey.fare).to eq Journey::PENALTY_FARE
+		end
+
+	end
 
 end
